594843ac6c3ed98dba8335b9c0381378e7554d0e
Jan Schöppach <jan.schoeppach@bestsens.de>
2017-09-14 14:36:35 +0200

    Push version [ci skip]

--- version.hpp
+++ version.hpp
@@ -7,11 +7,11 @@
 #define APP_VERSION_GITREV
 #endif
 
-#undef APP_DEV
+#define APP_DEV
 
 #define APP_VERSION_MAJOR   1
 #define APP_VERSION_MINOR   0
-#define APP_VERSION_PATCH   0
+#define APP_VERSION_PATCH   1
 
 #define APP_STR_EXP(__A)    #__A
 #define APP_STR(__A)        APP_STR_EXP(__A)
